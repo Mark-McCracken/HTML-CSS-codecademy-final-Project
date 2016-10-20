5f826a4cbb24803dc39195a7e80ee36aa5d2fe57
Mark McCracken <markmccracken91@me.com>
2016-10-20 15:34:51 +0100

    Initial commit

---
+++ pages/practice-pages/11_forms/js/11_script.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Mark on 12/05/2016.
+ */
+var verifyFirstName = function() {
+
+};
+
+
+
+
+var main = function() {
+    $('.btn').onclick(function() {
+
+    });
+};
+
+
+$(document).ready(main);
